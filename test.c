a483d736f98a0b8b76729f772418c270ce685054
epicLevi <tomas.levi@gmail.com>
2020-06-16 03:58:42 -0500

    Added test file.

---
+++ test.c
@@ -0,0 +1,19 @@
+int main () {
+	int int_var = 0;
+    int_var++;
+    int_var--;
+
+    float float_var = 0.1;
+
+    char str_var[] = "c";
+
+    if (int_var < 10 || int_var >= 0) {
+        while (float_var <= 1) {
+            printf("Hola, mundo.");
+        }
+
+        int tmp_int_var = int_var - 10 + 5 * 15 / 1;
+    }
+}
+
+void method() { }
