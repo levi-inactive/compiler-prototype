5a7b58fee0ba6a2f4f1f5f3b7c8072e1d24aaf43
epicLevi <tomas.levi@gmail.com>
2020-06-16 04:03:19 -0500

    Added YYTOKENTYPE definition.

--- scanner.h
+++ scanner.h
@@ -1,3 +1,41 @@
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+#define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+        OPEN_CURLY_BRACKET = 258,
+        CLOSE_CURLY_BRACKET = 259,
+        OPEN_BRACKET = 260,
+        CLOSE_BRACKET = 261,
+        INCREMENT = 262,
+        DECREMENT = 263,
+        LESS_OR_EQUAL = 264,
+        GREATER_OR_EQUAL = 265,
+        EQUAL = 266,
+        NOT_EQUAL = 267,
+        CLASS = 268,
+        ELSE = 269,
+        IF = 270,
+        INT = 271,
+        FLOAT = 272,
+        CHAR = 273,
+        VOID = 274,
+        DOUBLE = 275,
+        FOR = 276,
+        WHILE = 277,
+        PRINTF = 278,
+        MAIN = 279,
+        STR_CONST = 281,
+        INT_CONST = 282,
+        TYPEID = 283,
+        OBJECTID = 284,
+        ERROR = 285,
+        OR = 286,
+        AND = 287
+   };
+#endif
+
 #define OPEN_CURLY_BRACKET 258
 #define CLOSE_CURLY_BRACKET 259
 #define OPEN_BRACKET 260
